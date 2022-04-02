96a8a5d27c492fbdeabdbee056110fab03e7994a
lbdm44 <lewis.miller.github@pm.me>
2022-04-02 15:22:22 -0700

    refactor: bump apollo and graphql and refactor accoringly

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,7 @@
-import { ApolloServer, IResolvers } from 'apollo-server';
-import { typeDefs } from './type-defs';
-import { books, characters } from './resolvers';
+import { ApolloServer } from 'apollo-server';
 
-// Resolvers define the technique for fetching the types defined in the
-// schema. This resolver retrieves books from the "books" array above.
-const resolvers: IResolvers = {
-  Book: books.Book,
-  Query: {
-    books: books.books,
-    characters,
-  },
-};
+import { typeDefs } from './type-defs';
+import { resolvers } from './resolvers';
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,2 +1,14 @@
-export { default as books } from './books';
-export { default as characters } from './characters';
+import { IResolvers } from '@graphql-tools/utils';
+
+import books from './books';
+import characters from './characters';
+
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+export const resolvers: IResolvers = {
+  Book: books.Book,
+  Query: {
+    books: books.books,
+    characters,
+  },
+};
